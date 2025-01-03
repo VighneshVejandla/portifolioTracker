6e60231ca5bd5cff9018b049969a9f7959f8b5ca
VighneshVejandla2003 <vighnesh.2003vejandla@gmail.com>
2025-01-03 21:06:49 +0530

    codes insertrd

--- backend/src/main/java/com/portfolio/App.java
+++ backend/src/main/java/com/portfolio/App.java
@@ -1,13 +1,14 @@
 package com.portfolio;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @EnableJpaRepositories("com.portfolio.repository")
+// @EntityScan("com.portfolio.model")
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
